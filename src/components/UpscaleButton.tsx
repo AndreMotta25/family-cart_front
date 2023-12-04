f727c7065934cc6dcdd6eddc124fdf122c117733
AndreMotta25 <andremotta16041998@gmail.com>
2023-12-04 15:37:19 -0300

    ✨ feat: using tooltip at the top of the button tree

--- src/components/UpscaleButton.tsx
+++ src/components/UpscaleButton.tsx
@@ -1,13 +1,17 @@
 import * as Chakra from '@chakra-ui/react'
 import { ReactElement } from 'react'
+import { Tooltip } from './Tooltip'
 
-type UpscaleProps = {
-    icon: ReactElement
+export type UpscaleProps = {
+    icon: ReactElement,
+    tooltip?: string,
 } & Chakra.IconButtonProps
 
-const UpscaleButton = ({icon,_hover, ...rest}:UpscaleProps) => {
+const UpscaleButton = ({icon,tooltip,_hover, ...rest}:UpscaleProps) => {
     return (
-        <Chakra.IconButton _hover={{transform:'scale(1.1);',bg:"teal.400",..._hover}} icon={icon} {...rest}/>
+        <Tooltip label={tooltip}>
+            <Chakra.IconButton _hover={{transform:'scale(1.1);',bg:"teal.400",..._hover}} icon={icon} {...rest}/>
+        </Tooltip>
     )
 }
 export {UpscaleButton}
--- src/components/UpscaleLink.tsx
+++ src/components/UpscaleLink.tsx
@@ -1,17 +1,21 @@
 import NextLink from 'next/link';
-import {Link, LinkProps} from '@chakra-ui/react';
+import {Link, LinkProps, forwardRef} from '@chakra-ui/react';
 import { ReactNode } from 'react';
 import { sizeButton } from './ItemList/Actions';
+import { Tooltip } from './Tooltip';
 
 type UpscaleLinkProps = {
     children: ReactNode;
+    tooltip?: string;
 } & LinkProps
 
-const UpscaleLink = ({children, _hover, ...rest}:UpscaleLinkProps) => {
+const UpscaleLink = ({children,tooltip, _hover, ...rest}:UpscaleLinkProps) => {
     return (
-        <Link as={NextLink} bg={"gray.100"} borderRadius={'0.4rem'} _hover={{bg:"teal.400", transition:"all 0.3 ease", transform: 'scale(1.1)',..._hover}} display={'flex'} alignItems={'center'} justifyContent={'center'} minW={sizeButton} minH={{base:"2.5rem", md: '2.5rem'}} {...rest}>
-            {children}
-        </Link>
+        <Tooltip label={tooltip}>
+            <Link as={NextLink} bg={"gray.100"} borderRadius={'0.4rem'} _hover={{bg:"teal.400", transition:"all 0.3 ease", transform: 'scale(1.1)',..._hover}} display={'flex'} alignItems={'center'} justifyContent={'center'} minW={sizeButton} minH={{base:"2.5rem", md: '2.5rem'}} {...rest}>
+                {children}
+            </Link>
+        </Tooltip>
     )
 }
 export {UpscaleLink}
