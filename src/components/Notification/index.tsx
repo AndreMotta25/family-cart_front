7a2783b886cafb8205053d5f6efd18c25ec05438
AndreMotta25 <andremotta16041998@gmail.com>
2024-03-22 17:15:36 -0300

    ✨ feat: group of notification components for the composite pattern

---
+++ src/components/Notification/index.tsx
@@ -0,0 +1,16 @@
+import NotificationRoot from './NotificationRoot'
+import NotificationContent from './NotificationContent'
+import NotificationAvatar from './NotificationAvatar'
+import NotificationAction from './NotificationAction'
+import NotificationDate from './NotificationDate'
+import { NotificationButton } from './NotificationButton'
+
+const Notification = {
+  Root: NotificationRoot,
+  Content: NotificationContent,
+  Avatar: NotificationAvatar,
+  Action: NotificationAction,
+  Date: NotificationDate,
+  Button: NotificationButton
+}
+export {Notification}
