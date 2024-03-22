6cd19200266d502ee3e493d2ae5dabfb71936f1c
AndreMotta25 <andremotta16041998@gmail.com>
2024-03-22 16:46:30 -0300

    ✨ feat: default pages of next system

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,61 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {ChakraProvider, useDisclosure} from '@chakra-ui/react'
+import {theme} from '../styles/theme'
+import { ReactElement, ReactNode, useEffect, useState } from 'react';
+import { NextPage } from 'next';
+import {SessionProvider, signOut, useSession} from 'next-auth/react'
+import { useRouter } from 'next/router';
+import { Auth } from '@/components/Auth';
+import { NotifyProvider } from '@/contexts/notify';
+import ModalShare from '@/components/ModalShare';
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+  auth?: boolean;
 }
+ 
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+
+// if (process.env.NODE_ENV === 'development') {
+//   if(typeof window === 'undefined') {
+//     const {workerServer} = require('../services/msws/server')
+//     workerServer.listen();
+//   }
+//   else {
+//     const { worker } = require('../services/msws/index')
+//     worker.start();
+//   }
+// }
+
+const queryClient = new QueryClient();
+
+const App = ({ Component, pageProps}: AppPropsWithLayout) => {
+    const getLayout = Component.getLayout || ((page) => page);    
+
+    return (
+            <SessionProvider session={pageProps.session}>
+              {Component.auth ? 
+              <ChakraProvider theme={theme}>
+                <Auth>
+                    <QueryClientProvider client={queryClient}>
+                      <NotifyProvider>
+                        {getLayout(<Component {...pageProps}/>)}
+                      </NotifyProvider>
+                    </QueryClientProvider>
+                </Auth>
+              </ChakraProvider>
+              : 
+              <ChakraProvider theme={theme}>
+                {getLayout(<Component {...pageProps} />)}
+              </ChakraProvider>}
+            </SessionProvider>          
+      )
+     
+}
+
+export default App ;
+
--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,23 +1,24 @@
 import Header from "@/components/Header/Header"
 import SideBar from "@/components/SideBar"
 import { useMedia } from "@/hooks/useMedia"
-import { Box, Flex, useBreakpointValue, useDisclosure, FlexProps } from "@chakra-ui/react"
+import { Box, Flex, useBreakpointValue, useDisclosure, FlexProps, keyframes } from "@chakra-ui/react"
 import { ReactElement, createContext, useState } from "react"
+import {motion} from 'framer-motion'
+import { animation } from "@/utils/animate"
 
 type ILayoutProps = FlexProps &  {
     children: ReactElement | ReactElement[];
-
 }
 
-// O contexto deveria ser usado acima do layout
+
 const Layout = ({children, ...rest}:ILayoutProps) => {
     const isMobile = useBreakpointValue({base: true, md: true, lg: false});
     const {isOpen,onClose,onOpen} = useDisclosure();
 
     return(
-        <>
+        <> 
             <Header isMobile={isMobile} openMenu={onOpen}/>
-            <Flex pr={"2rem"} mt={"7.25rem"} {...rest}>
+            <Flex pr={"2rem"} mt={"7.25rem"} as={motion.div} animation={animation}{...rest}>
                 <SideBar isOpen={isOpen} isMobile={isMobile} closeMenu={onClose}/>
                 {children}
             </Flex>
