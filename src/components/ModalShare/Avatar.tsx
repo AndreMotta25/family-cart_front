4bc27783f76dc8e64cf70f498a5bd9af93dc9ebe
AndreMotta25 <andremotta16041998@gmail.com>
2024-03-22 18:40:06 -0300

    ♻️ refactor: using tooltip with avatar

--- src/components/ModalShare/Avatar.tsx
+++ src/components/ModalShare/Avatar.tsx
@@ -1,27 +1,30 @@
 import { Avatar as AvatarChakra, AvatarProps as AvatarChakraProps, forwardRef } from "@chakra-ui/react";
-import { ReactElement } from "react";
+import { ReactElement, ReactNode } from "react";
+import { AvatarTooltip } from "../AvatarTooltip";
 
 
 
 export type AvatarProps = {
-    children?: ReactElement
+    children?: ReactNode | ReactElement;
+    label?: string
 } & AvatarChakraProps;
 
-// forwardRef é usado para que possamos passar o componente avatar para o componente MenuButton
-const Avatar = forwardRef<AvatarProps,'span'>(({src, name,children, ...rest}, ref) => (
-     (<AvatarChakra size={"lg"} color={"black"}
-        _after={{w:'100%', h:"inherit", bg:"blackAlpha.600", borderRadius:"999999", position:'absolute', display:'block', content:'""', zIndex:1, opacity:0, transition:"all 0.3s ease"}}
+const Avatar = forwardRef<AvatarProps,'span'>(({src,label, name,children, ...rest}, ref) => {    
+    return (<AvatarTooltip size={"lg"} color={"black"}
+        _after={{top:0,w:'100%', h:"inherit", bg:"blackAlpha.600", borderRadius:"999999", position:'absolute', display:'block', content:'""', zIndex:2, opacity:0, transition:"all 0.3s ease"}}
         _hover={{_after:{opacity:1}}}
         name={name}
         src={src}
         ref={ref}
+        tooltipProps={{label: label, placement:"bottom", my: '-0.5rem'}}
         {...rest}
     >
         {children}
-    </AvatarChakra>)
-))
+    </AvatarTooltip>)
+})
 
 export {Avatar}
+
 
 
 // O heigth do after esta como inherit assim vai herdar a altura do pai. 
