f3b6ae4b8e848a62c50ddcff19468b3b79c4a0f9
AndreMotta25 <andremotta16041998@gmail.com>
2024-03-22 19:49:34 -0300

    ♻️ refactor: decreasing prop drilling 2

--- src/components/ItemList/Actions.tsx
+++ src/components/ItemList/Actions.tsx
@@ -4,17 +4,20 @@ import { useBreakpointValue } from '@chakra-ui/react';
 import { EditButton } from '../List/EditButton';
 import { TrashButton } from '../List/TrashButton';
 import { ExternalLink } from './ExternalLink';
+import { useListChanges } from '@/contexts/listChanges';
 
 export const sizeButton = {base:"2rem", md: '2.5rem'};
 
-const ActionsItem = ({item, removeItem, updateItem}:IItemProps) => {
+const ActionsItem = ({item,  updateItem}:IItemProps) => {
+  const {excludeItem: removeItem} = useListChanges()
+
     return (
     <Chakra.HStack spacing={"0.5rem"}>
       <EditButton aria-label='edit-item' color={"black"} _hover={{color:"white"}} bg={"gray.100"} minW={sizeButton} minH={sizeButton} onClick={() =>  updateItem(item.id as string)}/>
       <TrashButton aria-label='delete-item' color={"black"} _hover={{color:"white"}} bg={"gray.100"} minW={sizeButton} minH={sizeButton} onClick={() =>  removeItem(item.id as string)}/>
 
-      {item.link && (
-        <ExternalLink href={item.link} color={"black"} _hover={{color:"white"}}/>
+      {item.url && (
+        <ExternalLink href={item.url} color={"black"} _hover={{color:"white"}}/>
       )}
     </Chakra.HStack>)
 }
