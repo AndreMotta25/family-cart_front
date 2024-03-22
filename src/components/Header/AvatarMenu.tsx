0e8fe4a4f14b54465eb03f1c397cb75a7fe52ce2
AndreMotta25 <andremotta16041998@gmail.com>
2024-03-22 19:10:00 -0300

    ♻️ refactor: using the session to get the user's photo

--- src/components/Header/AvatarMenu.tsx
+++ src/components/Header/AvatarMenu.tsx
@@ -1,9 +1,14 @@
 import { forwardRef } from '@chakra-ui/react'
 import { Avatar as ComponentAvatar, AvatarProps} from '../ModalShare/Avatar'
+import { useSession } from 'next-auth/react'
 
 
 // forwardRef é usado para que possamos passar o componente avatar para o componente MenuButton
-const AvatarMenu = forwardRef<AvatarProps,'span'>((props,ref) => (
-    <ComponentAvatar cursor={"pointer"} boxSize={{base:"54px",lg: "84px"}} src='https://avatars.githubusercontent.com/u/28240286?v=4' name='Andre Motta' ref={ref} {...props}/>
-))
+const AvatarMenu = forwardRef<AvatarProps,'button'>((props,ref) => {
+    const {data: session} = useSession();
+
+    return (
+        <ComponentAvatar cursor={"pointer"} boxSize={{base:"54px",lg: "84px"}} src={session?.user.image} name={session?.user.name} ref={ref} {...props}/>
+    )})
+
 export {AvatarMenu}
