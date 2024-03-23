7622d3e3deb0f75813eca0d6495f1d63aacce1d0
AndreMotta25 <andremotta16041998@gmail.com>
2024-03-22 23:41:46 -0300

    ♻️ refactor: improving actions

--- src/components/Header/ActionsHeader.tsx
+++ src/components/Header/ActionsHeader.tsx
@@ -1,25 +1,24 @@
 import {HStack } from "@chakra-ui/react";
 import React from "react";
-import AddFriendIcon from "../Icons/AddFriendIcon";
 
-import AddLink from "../AddLink";
 import { useMedia } from "@/hooks/useMedia";
 
-import { UpscaleButton } from "../UpscaleButton";
 import { Notify } from "./Notify";
+
+import { CreateListButton } from "./CreateListButton";
+import { AddFriendButton } from "./AddFriendButton";
 
 const ActionsHeader = () => {
   const {isMatch: isWide} = useMedia('(min-width:62em)')
 
   return (
     <HStack mr={"1.25rem"}>
-      <UpscaleButton tooltip="Adicionar Amigo" aria-label="Add friend" _hover={{bg:"whiteAlpha.900"}} icon={<AddFriendIcon w={"44px"} height={"44px"}/>} bg={"transparent"} color={"teal.200"} px={"0.5rem"} position={"relative"}/>
+      <AddFriendButton/>
       <Notify/>
-      <AddLink/>
+      <CreateListButton/>
     </HStack>
   );
 };
 
 export default ActionsHeader;
 
-// mexer no AddLink.
