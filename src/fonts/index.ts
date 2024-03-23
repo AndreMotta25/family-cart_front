1b6d877bdfb46bf0f77ec451ba66168324aaa5ed
AndreMotta25 <andremotta16041998@gmail.com>
2024-03-22 23:42:18 -0300

    🧹 chore: add font dm_serif_display

---
+++ src/fonts/index.ts
@@ -0,0 +1,5 @@
+import {DM_Serif_Display} from 'next/font/google';
+
+const dm_serif_display = DM_Serif_Display({weight:["400"], variable:"--font-dm", subsets: ['latin']})
+
+export {dm_serif_display}
