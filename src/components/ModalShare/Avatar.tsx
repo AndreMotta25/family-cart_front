eea41c2d07eb2d5114f424c4b28b09dea2b16a50
AndreMotta25 <andremotta16041998@gmail.com>
2024-03-29 13:26:48 -0300

    ♻️ refactor: receiving hover

--- src/components/ModalShare/Avatar.tsx
+++ src/components/ModalShare/Avatar.tsx
@@ -9,10 +9,10 @@ export type AvatarProps = {
     label?: string
 } & AvatarChakraProps;
 
-const Avatar = forwardRef<AvatarProps,'span'>(({src,label, name,children, ...rest}, ref) => {    
+const Avatar = forwardRef<AvatarProps,'span'>(({src,label, name,children,_hover, ...rest}, ref) => {    
     return (<AvatarTooltip size={"lg"} color={"black"}
         _after={{top:0,w:'100%', h:"inherit", bg:"blackAlpha.600", borderRadius:"999999", position:'absolute', display:'block', content:'""', zIndex:2, opacity:0, transition:"all 0.3s ease"}}
-        _hover={{_after:{opacity:1}}}
+        _hover={{_after:{opacity:1}, ..._hover}}
         name={name}
         src={src}
         ref={ref}
